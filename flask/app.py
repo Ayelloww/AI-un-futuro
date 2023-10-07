c052d6a5afee5e768e02cc0be153ad9df95d4b85
IChrono <110775233+IChrono@users.noreply.github.com>
2023-10-08 00:46:02 +0200

    Ora vede css e js

--- flask/app.py
+++ flask/app.py
@@ -1,22 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route("/send", methods=["POST"])
-def send_message():
-    user_message = request.json.get("message")
-    
-    # Simuliamo una risposta AI statica
-    ai_response = "Ciao, sono l'AI. Ho ricevuto il tuo messaggio: " + user_message
-    
-    return jsonify({"message": ai_response})
-
-if __name__ == "__main__":
     app.run(debug=True)
---
+++ flask/static/js/main.js
@@ -0,0 +1,13 @@
+document.getElementById("color-button").addEventListener("click", function() {
+    const button = document.getElementById("color-button");
+    button.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
